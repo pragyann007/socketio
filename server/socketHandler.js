820092d894de7385a86822192601326bba66cd94
pragyann007 <pragyanthapaliya027@gmail.com>
2025-09-11 07:12:59 +0545

    handler coded

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,13 +1,26 @@
-const roomusers = [] ; 
+const roomusers = {} ; 
 export const handleSocket = (socket,io)=>{
     console.log("User connected", socket.id);
+
     socket.on("joinroom",(roomID)=>{
+
         socket.join(roomID);
+
         socket.roomID = roomID;
-    if(!roomusers[roomID]) roomID[roomID]={}
+
+    if(!roomusers[roomID]) roomusers[roomID]={}
     roomusers[roomID][socket.id]={} 
 
     })
 
+    socket.on("locationupdate",async (data)=>{
+        const {lat,long} = data ;
+        const roomID = socket.roomID ;
+
+        if(!roomID) return ; 
+
+        roomusers[roomID][socket.id] = {lat,long};
+    })
+   
 }
 
